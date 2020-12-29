26f12595bef237d1a725e9d16f6f33c8de8efbc1
natanael.leite <natansl@gmail.com>
2020-12-29 19:17:53 -0300

    feat: Adiciona metodo de excluir e desfazer alem do snackBar

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -85,31 +88,56 @@ class _HomeState extends State<Home> {
 
   Widget buildItem(context, index) {
     return Dismissible(
-        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-        background: Container(
-          color: Colors.red,
-          child: Align(
-            alignment: Alignment(-0.9, 0.0),
-            child: Icon(
-              Icons.delete,
-              color: Colors.white,
-            ),
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
-        direction: DismissDirection.startToEnd,
-        child: CheckboxListTile(
-            title: Text(_toDoList[index]['title']),
-            value: _toDoList[index]['ok'],
-            secondary: CircleAvatar(
-              child: Icon(_toDoList[index]['ok'] ? Icons.check : Icons.error),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+          title: Text(_toDoList[index]['title']),
+          value: _toDoList[index]['ok'],
+          secondary: CircleAvatar(
+            child: Icon(_toDoList[index]['ok'] ? Icons.check : Icons.error),
+          ),
+          onChanged: (c) {
+            print(c);
+            setState(() {
+              _toDoList[index]['ok'] = c;
+              _saveData();
+            });
+          }),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa ${_lastRemoved["title"]} removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
             ),
-            onChanged: (c) {
-              print(c);
-              setState(() {
-                _toDoList[index]['ok'] = c;
-                _saveData();
-              });
-            }));
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 
   Future<File> _getFile() async {
